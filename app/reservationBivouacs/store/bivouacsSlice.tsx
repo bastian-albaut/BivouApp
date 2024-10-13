7636cb08b618b54466de6d7f654ae8d99893171a
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-10-13 20:40:45 +0200

    🔥 refactor: Remove code about redux store in ReservationBivouacs

--- app/reservationBivouacs/store/bivouacsSlice.tsx
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getBivouacById } from '../api/bivouacs';
-
-export const fetchBivouacById = createAsyncThunk(
-  'bivouacs/fetchBivouacById',
-  async (id: number) => {
-    return getBivouacById(id);
-  }
-);
