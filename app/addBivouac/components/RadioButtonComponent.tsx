e5169f150a220fb04d00592d2cb81e2d67623b5e
Lénaïs Desbos <lenaisdesbos@MacBook-Air-de-Lenais.local>
2024-10-19 16:39:58 +0200

    add RadioButton Component

---
+++ app/addBivouac/components/RadioButtonComponent.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import Colors from "@/common/constants/Colors";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+export default function RadioButtonComponent(props: any) {
+  const { selected, label, onPress } = props;
+
+  return (
+    <TouchableOpacity style={styles.radioButtonContainer} onPress={onPress}>
+      <View style={[styles.radioButton, selected && styles.radioButtonSelected]}>
+        {selected && <View style={styles.radioInnerCircle} />}
+      </View>
+      <Text style={styles.radioButtonLabel}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  radioButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 20,
+  },
+  radioButton: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.black,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  radioButtonSelected: {
+    borderColor: Colors.green2,
+    borderWidth: 2,
+  },
+  radioButtonLabel: {
+    marginLeft: 10,
+    fontSize: 16,
+    color: Colors.black,
+  },
+  radioInnerCircle: {
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    backgroundColor: Colors.green2,
+  },
+});
