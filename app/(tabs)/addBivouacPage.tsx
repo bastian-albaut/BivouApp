4160b90c6f192028272330b8b470d87cb25e6914
lenais-desbos <lenais.desbos@etu.umontpellier.fr>
2024-11-16 20:17:07 +0100

    fix navigation

--- app/(tabs)/addBivouacPage.tsx
+++ app/(tabs)/addBivouacPage.tsx
@@ -1,6 +1,11 @@
 import AddStack from "@/app/addBivouac/screens/addStack";
+import { NavigationContainer } from "@react-navigation/native";
 
 // Tout doit etre migrer dans la page specifique dans features/...
 export default function AddBivouacPage() {
-  return <AddStack />;
+  return (
+    <NavigationContainer>
+      <AddStack />
+    </NavigationContainer>
+  );
 }
