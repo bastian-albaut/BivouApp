1ace6f91057bddee674bf408ad520ff506404d68
lenais-desbos <lenais.desbos@etu.umontpellier.fr>
2024-11-23 19:25:08 +0100

    update createBivouac

--- common/api/bivouac/bivouacs.tsx
+++ common/api/bivouac/bivouacs.tsx
@@ -1,5 +1,8 @@
 import { apiClient } from '../index';
 import { mockBivouacs } from './MockData';
+// import axios from 'axios';
+
+// const API_URL = 'http://localhost:8080/api';
 
 export const getBivouacs = async () => {
   // return await apiClient('bivouacs');
@@ -12,9 +15,37 @@ export const getBivouacById = async (id: number) => {
 };
 
 export const createBivouac = async (bivouacData: any) => {
-  return await apiClient('bivouacs', {
-    method: 'POST',
-    body: JSON.stringify(bivouacData),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  try {
+    const response = await apiClient('bivouacs', {
+      method: 'POST',
+      body: JSON.stringify(bivouacData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response;
+  } catch (error) {
+    console.error('Error creating bivouac:', error);
+    throw error;
+  }
 };
+
+// export const getBivouacsTest = async () => {
+//   const response = await axios.get(`${API_URL}/bivouacs`);
+//   return response.data;
+// };
+
+// export const getUserBivouacsTest = async () => {
+//   const response = await axios.get(`${API_URL}/user/bivouacs`);
+//   return response.data;
+// };
+
+// export const getBivouacByIdTest = async (id: number) => {
+//   const response = await axios.get(`${API_URL}/bivouacs/${id}`);
+//   return response.data;
+// };
+
+// export const createBivouacTest = async (bivouac: any) => {
+//   const response = await axios.post(`${API_URL}/bivouacs`, bivouac);
+//   return response.data;
+// };
