60f31357ec097f5845b6805c1e640c48f88ec136
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-11-24 17:58:57 +0100

    ✨ feat: Update API client base URL to Bivouapp API gateway

--- common/api/index.tsx
+++ common/api/index.tsx
@@ -1,7 +1,6 @@
 export const apiClient = async (endpoint: string, options: RequestInit = {}) => {
-  const baseUrl = 'https://jsonplaceholder.typicode.com';
-  // const response = await fetch(`${baseUrl}/${endpoint}`, options);
-  const response = await fetch(`${baseUrl}/users`, options);
+  const baseUrl = 'http://bivouapp-api-gateway.cluster-ig5.igpolytech.fr:8081/api';
+  const response = await fetch(`${baseUrl}/${endpoint}`, options);
   if (!response.ok) {
     throw new Error(`Error: ${response.body}`);
   }
