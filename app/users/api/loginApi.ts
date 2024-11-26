adb0897732c0e7631b10ad411a12c98f3c7d5c75
lenais-desbos <lenais.desbos@etu.umontpellier.fr>
2024-11-26 17:37:23 +0100

    fix login route

--- app/users/api/loginApi.ts
+++ app/users/api/loginApi.ts
@@ -2,7 +2,7 @@
 const BASE_URL = 'http://ms-user.cluster-ig5.igpolytech.fr:8080/api';
 
 export const loginApi = async (email: string, password: string): Promise<{ token: string; userId: number }> => {
-  const response = await fetch(`${BASE_URL}/login`, {
+  const response = await fetch(`${BASE_URL}/users/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
